5d34583a795d434e01f768366a754f17435bd78e
Alistair Burrowes <alistair.burrowes@gmail.com>
2017-08-06 13:31:05 +1000

    Move implementation

--- src/BoardProcessor.hs
+++ src/BoardProcessor.hs
@@ -8,8 +8,11 @@ import Data.Maybe (fromJust)
 
 execute :: T.Command -> T.Board -> T.Board
 execute (T.Place x y facing) = set T.boardRobot (Just $ T.Robot x y facing)
-execute T.Right = over (T.boardRobot . _Just . T.robotFacing) $ adjustDirection 1
-execute T.Left  = over (T.boardRobot . _Just . T.robotFacing) $ adjustDirection (-1)
+execute T.Right = over (placedRobot . T.robotFacing) $ adjustDirection 1
+execute T.Left  = over (placedRobot. T.robotFacing) $ adjustDirection (-1)
+execute T.Move = over placedRobot move
+
+placedRobot = T.boardRobot . _Just
 
 directions :: [T.Direction]
 directions = [T.North, T.East, T.South, T.West]
@@ -19,3 +22,9 @@ getIndex = fromJust . flip elemIndex directions
 
 adjustDirection :: Int -> T.Direction -> T.Direction
 adjustDirection num = (!!) directions . flip mod 4 . fromIntegral . (+ num) . getIndex
+
+move :: T.Robot -> T.Robot
+move r@(T.Robot _ _ T.North) = over T.robotY (+1) r
+move r@(T.Robot _ _ T.East) = over T.robotX (+1) r
+move r@(T.Robot _ _ T.South) = over T.robotY (flip (-) 1) r
+move r@(T.Robot _ _ T.West) = over T.robotX (flip (-) 1) r
