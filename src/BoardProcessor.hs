d614ff043a529101da81f4f6734a20e5d1153fce
Alistair Burrowes <alistair.burrowes@gmail.com>
2017-08-06 13:11:38 +1000

    Make adjustDirection have less knowledge

--- src/BoardProcessor.hs
+++ src/BoardProcessor.hs
@@ -2,14 +2,14 @@ module BoardProcessor where
 
 import qualified Types as T
 
-import Control.Lens (over, set)
+import Control.Lens (over, set, _Just)
 import Data.List (elemIndex)
 import Data.Maybe (fromJust)
 
 execute :: T.Command -> T.Board -> T.Board
 execute (T.Place x y facing) = set T.boardRobot (Just $ T.Robot x y facing)
-execute T.Right = over T.boardRobot $ fmap (adjustDirection 1)
-execute T.Left  = over T.boardRobot $ fmap (adjustDirection (-1))
+execute T.Right = over (T.boardRobot . _Just . T.robotFacing) $ adjustDirection 1
+execute T.Left  = over (T.boardRobot . _Just . T.robotFacing) $ adjustDirection (-1)
 
 directions :: [T.Direction]
 directions = [T.North, T.East, T.South, T.West]
@@ -17,7 +17,5 @@ directions = [T.North, T.East, T.South, T.West]
 getIndex :: T.Direction -> Int
 getIndex = fromJust . flip elemIndex directions
 
-adjustDirection :: Int -> T.Robot -> T.Robot
-adjustDirection = over T.robotFacing . newIndex
-  where
-    newIndex num = (!!) directions . flip mod 4 . fromIntegral . (+ num) . getIndex
+adjustDirection :: Int -> T.Direction -> T.Direction
+adjustDirection num = (!!) directions . flip mod 4 . fromIntegral . (+ num) . getIndex
