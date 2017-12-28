b1ac908996af014cbf0ca2694efdee4c892c1efa
Alistair Burrowes <alistair.burrowes@gmail.com>
2017-12-28 15:41:20 +1100

    Still playing with BoardProcessorSpec

--- test/BoardProcessorSpec.hs
+++ test/BoardProcessorSpec.hs
@@ -5,8 +5,9 @@ import Control.Monad.State (execStateT)
 import Control.Monad.Writer (runWriter)
 -- import Test.QuickCheck (forAll, property)
 import Test.QuickCheck.Gen (generate)
+import Control.Lens (set, _Just)
 
-import Generators (genBoardNoRobot, genPlaceCommandWithin)
+import Generators (genBoardNoRobot, genPlaceCommandWithin, genBoardValidRobot)
 import BoardProcessor (getAction)
 import qualified Types as T
 
@@ -23,9 +24,27 @@ spec = describe "BoardProcessor" $ do
       result `shouldBe` expected
 
   describe "leftAction" $
-    it "performs a left" $
-      let action        = getAction $ T.TurnLeft
+    it "performs a left" $ do
+      board@(T.Board bc (Just (T.Robot rc _))) <- generate genBoardValidRobot
+      let testBoard     = set (T.boardRobot . _Just . T.robotFacing) T.North board
+          action        = getAction T.TurnLeft
+          (result, _)   = runWriter $ execStateT action testBoard
+          expected      = T.Board bc (Just $ T.Robot rc T.West)
+      result `shouldBe` expected
+
+  describe "rightAction" $
+    it "performs a right" $ do
+      board@(T.Board bc (Just (T.Robot rc _))) <- generate genBoardValidRobot
+      let testBoard     = set (T.boardRobot . _Just . T.robotFacing) T.North board
+          action        = getAction T.TurnRight
+          (result, _)   = runWriter $ execStateT action testBoard
+          expected      = T.Board bc (Just $ T.Robot rc T.East)
+      result `shouldBe` expected
+
+  describe "moveAction" $
+    it "performs a move" $
+      let action        = getAction $ T.Move
           board         = T.Board (T.Coordinate 5 5) (Just $ T.Robot (T.Coordinate 2 2) T.North)
           (result, _)   = runWriter $ execStateT action board
-          expected      = T.Board (T.Coordinate 5 5) (Just $ T.Robot (T.Coordinate 2 2) T.West)
+          expected      = T.Board (T.Coordinate 5 5) (Just $ T.Robot (T.Coordinate 2 3) T.North)
       in  result `shouldBe` expected
