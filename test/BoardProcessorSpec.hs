cdee58dc358eb98aefbfbed0ec93c991e40fbe87
Alistair Burrowes <alistair.burrowes@gmail.com>
2017-12-28 15:51:08 +1100

    Add report test, more cleanup

--- test/BoardProcessorSpec.hs
+++ test/BoardProcessorSpec.hs
@@ -1,15 +1,15 @@
 module BoardProcessorSpec where
 
-import Test.Hspec (Spec, describe, it, shouldBe)
-import Control.Monad.State (execStateT)
-import Control.Monad.Writer (runWriter)
--- import Test.QuickCheck (forAll, property)
-import Test.QuickCheck.Gen (generate)
-import Control.Lens (set, _Just)
+import           Control.Lens         (set, _Just)
+import           Control.Monad.State  (execStateT)
+import           Control.Monad.Writer (execWriter, runWriter)
+import           Test.Hspec           (Spec, describe, it, shouldBe)
+import           Test.QuickCheck.Gen  (generate)
 
-import Generators (genBoardNoRobot, genPlaceCommandWithin, genBoardValidRobot)
-import BoardProcessor (getAction)
-import qualified Types as T
+import           BoardProcessor       (getAction)
+import           Generators           (genBoardNoRobot, genBoardValidRobot,
+                                       genPlaceCommandWithin)
+import qualified Types                as T
 
 spec :: Spec
 spec = describe "BoardProcessor" $ do
@@ -18,33 +18,41 @@ spec = describe "BoardProcessor" $ do
     it "performs a place" $ do
       board@(T.Board bc@(T.Coordinate x y) _) <- generate genBoardNoRobot
       command@(T.Place cc cdir)               <- generate $ genPlaceCommandWithin x y
-      let action        = getAction command
-          (result, _)   = runWriter $ execStateT action board
-          expected      = T.Board bc (Just $ T.Robot cc cdir)
+      let action      = getAction command
+          (result, _) = runWriter $ execStateT action board
+          expected    = T.Board bc (Just $ T.Robot cc cdir)
       result `shouldBe` expected
 
   describe "leftAction" $
     it "performs a left" $ do
       board@(T.Board bc (Just (T.Robot rc _))) <- generate genBoardValidRobot
-      let testBoard     = set (T.boardRobot . _Just . T.robotFacing) T.North board
-          action        = getAction T.TurnLeft
-          (result, _)   = runWriter $ execStateT action testBoard
-          expected      = T.Board bc (Just $ T.Robot rc T.West)
+      let testBoard   = set (T.boardRobot . _Just . T.robotFacing) T.North board
+          action      = getAction T.TurnLeft
+          (result, _) = runWriter $ execStateT action testBoard
+          expected    = T.Board bc (Just $ T.Robot rc T.West)
       result `shouldBe` expected
 
   describe "rightAction" $
     it "performs a right" $ do
       board@(T.Board bc (Just (T.Robot rc _))) <- generate genBoardValidRobot
-      let testBoard     = set (T.boardRobot . _Just . T.robotFacing) T.North board
-          action        = getAction T.TurnRight
-          (result, _)   = runWriter $ execStateT action testBoard
-          expected      = T.Board bc (Just $ T.Robot rc T.East)
+      let testBoard   = set (T.boardRobot . _Just . T.robotFacing) T.North board
+          action      = getAction T.TurnRight
+          (result, _) = runWriter $ execStateT action testBoard
+          expected    = T.Board bc (Just $ T.Robot rc T.East)
       result `shouldBe` expected
 
   describe "moveAction" $
     it "performs a move" $
-      let action        = getAction $ T.Move
-          board         = T.Board (T.Coordinate 5 5) (Just $ T.Robot (T.Coordinate 2 2) T.North)
-          (result, _)   = runWriter $ execStateT action board
-          expected      = T.Board (T.Coordinate 5 5) (Just $ T.Robot (T.Coordinate 2 3) T.North)
+      let action      = getAction $ T.Move
+          board       = T.Board (T.Coordinate 5 5) (Just $ T.Robot (T.Coordinate 2 2) T.North)
+          (result, _) = runWriter $ execStateT action board
+          expected    = T.Board (T.Coordinate 5 5) (Just $ T.Robot (T.Coordinate 2 3) T.North)
+      in  result `shouldBe` expected
+
+  describe "reportAction" $
+    it "produces a report" $
+      let action   = getAction $ T.Report
+          board    = T.Board (T.Coordinate 5 5) (Just $ T.Robot (T.Coordinate 2 2) T.North)
+          result   = execWriter $ execStateT action board
+          expected = ["2,2,NORTH"]
       in  result `shouldBe` expected
