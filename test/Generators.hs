ffb45f04d6c8c7aa86186e9958ce5080973b43dd
Alistair Burrowes <alistair.burrowes@gmail.com>
2018-01-13 11:36:05 +1100

    Pure over return in generators. Also use Enum for direction list

--- test/Generators.hs
+++ test/Generators.hs
@@ -6,55 +6,55 @@ import           Test.QuickCheck.Gen (choose)
 import qualified Types               as T
 
 genDirection :: Gen T.Direction
-genDirection = elements [T.North, T.East, T.South, T.West]
+genDirection = elements [toEnum 0 ..]
 
 genRobot :: Gen T.Robot
 genRobot = do
   c <- genCoordinate
   f <- genDirection
-  return $ T.Robot c f
+  pure $ T.Robot c f
 
 genCoordinate :: Gen T.Coordinate
 genCoordinate = do
   x <- choose (1, 5)
   y <- choose (1, 5)
-  return $ T.Coordinate x y
+  pure $ T.Coordinate x y
 
 genRobotWithin :: Int -> Int -> Gen T.Robot
 genRobotWithin x y = do
   x' <- choose (1, x)
   y' <- choose (1, y)
   f  <- genDirection
-  return $ T.Robot (T.Coordinate x' y') f
+  pure $ T.Robot (T.Coordinate x' y') f
 
 genRobotGreaterThan :: Int -> Int -> Gen T.Robot
 genRobotGreaterThan x y = do
   x' <- choose (x + 1, maxBound)
   y' <- choose (y + 1, maxBound)
   f  <- genDirection
-  return $ T.Robot (T.Coordinate x' y') f
+  pure $ T.Robot (T.Coordinate x' y') f
 
 genBoardValidRobot :: Gen T.Board
 genBoardValidRobot = do
   c <- genCoordinate
   r <- genRobotWithin (T._coordinateX c) (T._coordinateY c)
-  return $ T.Board c (Just r)
+  pure $ T.Board c (Just r)
 
 genBoardInvalidRobot :: Gen T.Board
 genBoardInvalidRobot = do
   c <- genCoordinate
   r <- genRobotGreaterThan (T._coordinateX c) (T._coordinateY c)
-  return $ T.Board c (Just r)
+  pure $ T.Board c (Just r)
 
 genBoardNoRobot :: Gen T.Board
 genBoardNoRobot = do
   c <- genCoordinate
-  return $ T.Board c Nothing
+  pure $ T.Board c Nothing
 
 genPlaceCommandWithin :: Int -> Int -> Gen T.Command
 genPlaceCommandWithin x y = do
   x' <- choose (1, x)
   y' <- choose (1, y)
   d  <- genDirection
-  return $ T.Place (T.Coordinate x' y') d
+  pure $ T.Place (T.Coordinate x' y') d
 
